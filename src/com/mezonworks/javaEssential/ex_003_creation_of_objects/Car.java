8b4ab08c6fed3b742a588f65e57a97c7037376b0
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 21:45:46 +0900

    003_creation_of_objects

---
+++ src/com/mezonworks/javaEssential/ex_003_creation_of_objects/Car.java
@@ -0,0 +1,6 @@
+package com.mezonworks.javaEssential.ex_003_creation_of_objects;
+
+public class Car {
+    //Reference type || name of variable || "new" - Instantiation || Constructor
+      Car               car               =  new                     Car();
+}
