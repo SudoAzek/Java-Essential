ce46e121605234c4899daec0990223e518927a1e
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:23:56 +0900

    Implicitly imported lang package

---
+++ src/com/mezonworks/javaEssential/ex_007_packages03_lang/Main.java
@@ -0,0 +1,9 @@
+package com.mezonworks.javaEssential.ex_007_packages03_lang;
+
+//import java.lang.*;
+
+public class Main {
+    // Java JVM imports java.lang.* implicitly so no need to import them explicitly.
+    String str = new String();
+    String str2 = "Hello";
+}
