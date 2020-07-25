3259a0097883be8517dde829823e69070d66d923
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:43:57 +0900

    Revert "Revert "Revert "Revert "Using static methods without static import""""
    This reverts commit a5e7ae0b1cfcdba7116356ea7abe5b8d1f895305.

---
+++ src/com/mezonworks/javaEssential/ex_010_packages06_import_static/Main.java
@@ -0,0 +1,14 @@
+package com.mezonworks.javaEssential.ex_010_packages06_import_static;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double number = 1;
+        double result = cos(PI * 1);
+
+        System.out.println(result);
+    }
+}
