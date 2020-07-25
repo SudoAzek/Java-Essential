4145ceba08e487dfbec6bd3b0cda92d4b6db2f9c
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 21:13:31 +0900

    Encapsulation - access modifiers
    Access modifiers in Java OOP

---
+++ src/com/mezonworks/javaEssential/package2/ExampleInPackage2.java
@@ -0,0 +1,8 @@
+package com.mezonworks.javaEssential.package2;
+
+public class ExampleInPackage2 {
+    private String name1 = "private";
+    String name2 = "package";
+    protected String name3 = "protected";
+    public String name4 = "public";
+}
