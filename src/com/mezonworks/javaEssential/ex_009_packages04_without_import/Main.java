e11fa7dd2476a8e21ef7eb5d91e7456470fe3274
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:33:11 +0900

    Using other package class without general import.

---
+++ src/com/mezonworks/javaEssential/ex_009_packages04_without_import/Main.java
@@ -0,0 +1,7 @@
+package com.mezonworks.javaEssential.ex_009_packages04_without_import;
+
+
+public class Main {
+    com.mezonworks.javaEssential.ex_001_class_declaration.Car myCar =
+            new com.mezonworks.javaEssential.ex_001_class_declaration.Car();
+}
