f251bbffeaad4dcacd580392761f20876a87800f
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:02:04 +0900

    Method calling

---
+++ src/com/mezonworks/javaEssential/ex_004_method_calling/Car.java
@@ -0,0 +1,38 @@
+package com.mezonworks.javaEssential.ex_004_method_calling;
+
+public class Car {
+    private String model;
+    private int maxSpeed;
+    private int year;
+    private int speed;
+
+    public Car() {
+    }
+
+    public Car(String model, int maxSpeed, int year, int speed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.year = year;
+        this.speed = speed;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
