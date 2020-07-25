6b189114d02fb6718d5913a1f7e941970b7d6128
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:31:28 +0900

    With imports

---
+++ src/com/mezonworks/javaEssential/ex_007_packages02_import/Main.java
@@ -0,0 +1,9 @@
+package com.mezonworks.javaEssential.ex_007_packages02_import;
+
+import com.mezonworks.javaEssential.ex_001_class_declaration.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+    }
+}
