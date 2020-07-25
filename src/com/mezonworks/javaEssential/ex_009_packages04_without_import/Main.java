8af954aeff6271d3353fcef00622827ccd3b7335
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-25 22:36:26 +0900

    Update Main.java

--- src/com/mezonworks/javaEssential/ex_009_packages04_without_import/Main.java
+++ src/com/mezonworks/javaEssential/ex_009_packages04_without_import/Main.java
@@ -2,6 +2,10 @@ package com.mezonworks.javaEssential.ex_009_packages04_without_import;
 
 
 public class Main {
-    com.mezonworks.javaEssential.ex_001_class_declaration.Car myCar =
-            new com.mezonworks.javaEssential.ex_001_class_declaration.Car();
+    public static void main(String[] args) {
+        com.mezonworks.javaEssential.ex_001_class_declaration.Car myCar =
+                new com.mezonworks.javaEssential.ex_001_class_declaration.Car();
+
+        System.out.println(myCar);
+    }
 }
