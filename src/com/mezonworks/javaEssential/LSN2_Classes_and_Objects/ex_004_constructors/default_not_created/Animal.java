26d6de0dfe2d374e84aa58ad13a9313a4f114f47
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 18:09:37 +0900

    Constructor overloading

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_004_constructors/default_not_created/Animal.java
@@ -0,0 +1,11 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_004_constructors.default_not_created;
+
+public class Animal {
+    private int age;
+
+    public Animal(int age) {
+        this.age = age;
+    }
+    public Animal(){
+    }
+}
---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_004_constructors/default_not_created/Main.java
@@ -0,0 +1,6 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_004_constructors.default_not_created;
+
+public class Main {
+    Animal animal = new Animal(11);
+    Animal animal2 = new Animal();
+}
