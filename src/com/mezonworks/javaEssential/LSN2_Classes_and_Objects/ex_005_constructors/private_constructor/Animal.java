7e888ec83ca4f9b9be7b9bc3a211f705d50f187b
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 18:13:31 +0900

    Private constructor

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_005_constructors/private_constructor/Animal.java
@@ -0,0 +1,9 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_005_constructors.private_constructor;
+
+public class Animal {
+    private int age;
+
+    private Animal(int age) {
+        this.age = age;
+    }
+}
---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_005_constructors/private_constructor/Main.java
@@ -0,0 +1,7 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_005_constructors.private_constructor;
+
+public class Main {
+    public static void main(String[] args) {
+//        Animal animal = new Animal(5);
+    }
+}
