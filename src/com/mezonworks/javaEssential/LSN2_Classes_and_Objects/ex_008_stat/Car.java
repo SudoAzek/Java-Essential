01dfc51801f13e8f19fc2b7d4acd60972ec27104
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 19:53:20 +0900

    Create Car.java
    Use of static context variable example

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_008_stat/Car.java
@@ -0,0 +1,21 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_008_stat;
+
+public class Car {
+    String model;
+    String color;
+
+    static double count;
+
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car.count = 1;
+
+        Car c2 = new Car();
+        Car.count = 2;
+
+        Car c100 = new Car();
+        Car.count = 100;
+
+        System.out.println(Car.count);
+    }
+}
