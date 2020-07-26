89352aec1cff00d6852a9ce61b6d107a3c6d39ff
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 17:45:50 +0900

    Create Animal.java
    example 2

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_002_constructors/keyword_this/Animal.java
@@ -0,0 +1,11 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_002_constructors.keyword_this;
+
+public class Animal {
+    private int age;
+    private int height;
+
+    public Animal(int age, int height) {
+        this.age = age;
+        this.height = height;
+    }
+}
