d0f9eae0e15ae87cc317b70c94b8fdf2bab6a6a6
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 17:06:11 +0900

    Create Animal.java

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_001_constructors/declaration/Animal.java
@@ -0,0 +1,10 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_001_constructors.declaration;
+
+public class Animal {
+    private int age;
+
+    // Constructor declaration
+    public Animal(int age) {
+        this.age = age;
+    }
+}
