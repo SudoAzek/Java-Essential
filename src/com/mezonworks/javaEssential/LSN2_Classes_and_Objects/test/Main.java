77d539d1822be24bbec9402dcc94f22f45f225da
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 16:50:09 +0900

    example test

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/test/Main.java
@@ -0,0 +1,10 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.setPrice(10000);
+        System.out.println(car.getPrice());
+    }
+}
