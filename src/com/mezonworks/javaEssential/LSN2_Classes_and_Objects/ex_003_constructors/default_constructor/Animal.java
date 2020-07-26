52a5799af6935ae91da189fce1c9d0897431f3c6
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-26 17:46:12 +0900

    Example 003

---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_003_constructors/default_constructor/Animal.java
@@ -0,0 +1,5 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_003_constructors.default_constructor;
+
+public class Animal {
+    private int age;
+}
---
+++ src/com/mezonworks/javaEssential/LSN2_Classes_and_Objects/ex_003_constructors/default_constructor/Main.java
@@ -0,0 +1,8 @@
+package com.mezonworks.javaEssential.LSN2_Classes_and_Objects.ex_003_constructors.default_constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(animal);
+    }
+}
