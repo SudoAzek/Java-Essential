97ad87734641506c2620d862de5d8bb49797cece
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-07-27 09:34:39 +0900

    Create Bird.java

---
+++ src/com/mezonworks/javaEssential/LSN3_Inheritance_and_Polymorphism/bird/Bird.java
@@ -0,0 +1,7 @@
+package com.mezonworks.javaEssential.LSN3_Inheritance_and_Polymorphism.bird;
+
+public class Bird {
+    public void move() {
+        System.out.println("Different ways");
+    }
+}
