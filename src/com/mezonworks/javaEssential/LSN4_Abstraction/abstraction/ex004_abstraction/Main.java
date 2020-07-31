ff1a4698475b321516eeebea1ccaa503cd30cfba
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-01 06:37:15 +0900

    Update Main.java

--- src/com/mezonworks/javaEssential/LSN4_Abstraction/abstraction/ex004_abstraction/Main.java
+++ src/com/mezonworks/javaEssential/LSN4_Abstraction/abstraction/ex004_abstraction/Main.java
@@ -2,11 +2,10 @@ package com.mezonworks.javaEssential.LSN4_Abstraction.abstraction.ex004_abstract
 
 public class Main {
     public static void main(String[] args) {
-    ConcreteDerivedClass instance = new ConcreteDerivedClass();
+        ConcreteDerivedClass instance = new ConcreteDerivedClass();
 
-    instance.simpleMethod();
-    instance.overriddenMethod();
-    instance.abstractMethod();
-
+        instance.simpleMethod();
+        instance.overriddenMethod();
+        instance.abstractMethod();
     }
 }
