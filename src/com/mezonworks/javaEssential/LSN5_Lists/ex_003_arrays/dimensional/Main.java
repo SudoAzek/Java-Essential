487928a050ad0442c7974b5c1a623b5e223472c8
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-01 20:05:53 +0900

    Arrays, Lists - example 03

---
+++ src/com/mezonworks/javaEssential/LSN5_Lists/ex_003_arrays/dimensional/Main.java
@@ -0,0 +1,13 @@
+package com.mezonworks.javaEssential.LSN5_Lists.ex_003_arrays.dimensional;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 15},
+                {31, 44, 99, 56},
+                {65, 12, 42},
+                {}
+        };
+        System.out.println(arr.length);
+    }
+}
