d19a654f2c2b07a3290f5505a153da0c3536701f
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-01 19:59:05 +0900

    Arrays, Lists - example 02

---
+++ src/com/mezonworks/javaEssential/LSN5_Lists/ex_002_arrays/rewrite/Main.java
@@ -0,0 +1,12 @@
+package com.mezonworks.javaEssential.LSN5_Lists.ex_002_arrays.rewrite;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        System.out.println(arr1.length);
+
+        arr1 = new int[25];
+        System.out.println(arr1.length);
+    }
+
+}
