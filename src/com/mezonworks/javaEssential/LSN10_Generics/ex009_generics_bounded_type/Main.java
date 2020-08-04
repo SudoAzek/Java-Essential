dc9561a4b73fb746b7aeabfba6a45b667146feaa
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-05 08:36:27 +0900

    ex009_generics_bounded_type

---
+++ src/com/mezonworks/javaEssential/LSN10_Generics/ex009_generics_bounded_type/Main.java
@@ -0,0 +1,28 @@
+package com.mezonworks.javaEssential.LSN10_Generics.ex009_generics_bounded_type;
+
+interface Interface1 {
+}
+
+interface Interface2<U> extends Interface1 {
+}
+
+class Derived implements Interface1, Interface2<Object> {
+}
+
+class Derived2 implements Interface2<Object> {
+}
+
+class MyClass<T extends Interface1> {
+}
+
+class MyClass2<T extends Interface2<Object>> {
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass<Interface2<Object>> my1 = new MyClass<>();
+        MyClass2<Derived> my2 = new MyClass2<>();
+        MyClass2<Derived2> my3 = new MyClass2<>();
+        MyClass2<Interface2<Object>> my4 = new MyClass2<>();
+    }
+}
