5b6cec5c196d93b4e28eee2e27c06d96515edb3a
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-04 10:31:58 +0900

    Comarison of raw and defined Lists

---
+++ src/com/mezonworks/javaEssential/LSN10_Generics/Main.java
@@ -0,0 +1,18 @@
+package com.mezonworks.javaEssential.LSN10_Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List l = new ArrayList();
+        List <String> list = new ArrayList<>();
+
+        l = list;
+
+        l.add("Hello");
+
+        String s = list.get(0);
+        System.out.println(s);
+    }
+}
