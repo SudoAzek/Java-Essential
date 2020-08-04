67b9668f4baba45acf9532f36108b944121d85b9
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2020-08-05 06:43:57 +0900

    ex005_generics

---
+++ src/com/mezonworks/javaEssential/LSN10_Generics/Test.java
@@ -0,0 +1,15 @@
+package com.mezonworks.javaEssential.LSN10_Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Number[] num = new Integer[10];
+        num[0] = 10.10d;
+
+        System.out.println(num[0]);
+
+//        List<Number> list = new ArrayList<Integer>();
+    }
+}
